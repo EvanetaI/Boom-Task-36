8df9596886c738336cb4431b6d8d4744043ec951
Evaneta <eftimovaevaneta@gmail.com>
2023-02-09 21:36:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import "./App.css";
+import { useMemo, useState } from "react";
 
 function App() {
+  let [value, setValue] = useState(false)
+  const pattern = new RegExp(/^\d+$/);
+  
+  const set =(e)=>{
+    setValue(e.target.value)
+  }
+
+  const memoizedValue = useMemo(() => {
+    if (pattern.test(value)) {
+      return true
+    } else {
+      return false
+    }
+  }, [value]);
+
   return (
     <div className="App">
      <div className="control has-icons-right">
-        <input
+        <input onChange={set}
           className="input is-large"
           type="text"
           placeholder="Enter number..."
 
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
+          <i className={memoizedValue ? "fa-check" : "fas fa-times"}/>
         </span> 
       </div>
     </div>
